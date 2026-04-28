d923f3f83621bf92165b721605d9dee78dd89265
Apirak Tooltham <apirak@npu.ac.th>
2026-04-29 01:49:37 +0700

    fix: hardcode db hostname and use boss password

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'db',
+        'username'     => 'user',
+        'password'     => '?@xfree86',
+        'database'     => 'elderly_survey',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
